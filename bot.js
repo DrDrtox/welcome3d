2824e28ab3914b6325cfc76521c1339ace92e2cb
drtox <45079652+drtox@users.noreply.github.com>
2018-11-15 22:17:39 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js'),
+
+    bot = new Discord.Client({sisableEveryone: true})
+
+    console.log("muuuuuuuuute,");
+
+bot.on('guildMemberAdd', member => {
+
+const mohamed= member.guild.channels.get("511875595773149185");
+
+if(!mohamed) return;
+
+if(mohamed) {
+
+setTimeout(() => mohamed.send(`**Welcomee To :rocket: Neptuunee , **`), 4000)        
+
+}
+
+});
+
+
+
+bot.login(process.env.BOT_TOKEN);
